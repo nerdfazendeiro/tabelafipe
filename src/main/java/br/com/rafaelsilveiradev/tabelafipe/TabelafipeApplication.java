3bd4ee69f5216a3c6e3cdcdbd143ae199856bf9a
Rafael Ramalho Silveira <rafael_silveira125@hotmail.com>
2024-04-23 06:03:10 -0400

    Consumindo API

--- src/main/java/br/com/rafaelsilveiradev/tabelafipe/TabelafipeApplication.java
+++ src/main/java/br/com/rafaelsilveiradev/tabelafipe/TabelafipeApplication.java
@@ -1,5 +1,6 @@
 package br.com.rafaelsilveiradev.tabelafipe;
 
+import br.com.rafaelsilveiradev.tabelafipe.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class TabelafipeApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/br/com/rafaelsilveiradev/tabelafipe/principal/Principal.java
+++ src/main/java/br/com/rafaelsilveiradev/tabelafipe/principal/Principal.java
@@ -1,9 +1,14 @@
 package br.com.rafaelsilveiradev.tabelafipe.principal;
 
+import br.com.rafaelsilveiradev.tabelafipe.services.ConsumoAPI;
+
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
+    private ConsumoAPI consumo = new ConsumoAPI();
+
+    private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
 
     public void exibeMenu(){
         System.out.println("**** Opções ****");
@@ -11,6 +16,9 @@ public class Principal {
         System.out.println("Motos");
         System.out.println("Caminhões");
         System.out.println("Digite o tipo de veiculo que deseja pesquisar: ");
+        var tipoVeiculo = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + tipoVeiculo + "/marcas");
+        System.out.println(json);
 
     }
 }
